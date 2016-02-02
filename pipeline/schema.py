676cf9b32e11030d60e183c52f43639e3eddd2d8
Ben Smithgall <ben.smithgall@gmail.com>
2016-02-02 10:45:58 -0500

    add automatic marshmallow --> CKAN field converter

--- pipeline/schema.py
+++ pipeline/schema.py
@@ -1,4 +1,29 @@
-from marshmallow import Schema
+from marshmallow import Schema, fields
+
+FIELD_TO_CKAN_TYPE_MAPPING = {
+    fields.String: 'text',
+    fields.Number: 'numeric', fields.Integer: 'numeric',
+    fields.DateTime: 'timestamp', fields.Date: 'timestamp'
+}
 
 class BaseSchema(Schema):
-    pass
+    def serialize_to_ckan_fields(self):
+        '''Convert schema fieldlist to CKAN-friendly Fields
+
+        Returns:
+            A list of dictionaries with proper name/type mappings
+            for CKAN. For example, name=fields.String() would go
+            to:
+                [
+                    {
+                        'id': 'name',
+                        'type': 'text'
+                    }
+                ]
+        '''
+        ckan_fields = []
+        for name, marsh_field in self.fields.items():
+            ckan_fields.append({
+                'id': name,
+                'type': FIELD_TO_CKAN_TYPE_MAPPING[marsh_field.__class__]
+            })
